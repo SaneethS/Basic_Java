45f123a017a66dd4e76663ef6f666453f8d2c441
Saneeth S <saneeths@125AMBBLR-Chethan-RV.local>
2021-08-24 23:14:04 +0530

    Sum of three numbers 0 is updated

--- src/com/functionalcode/SumOfThree.java
+++ src/com/functionalcode/SumOfThree.java
@@ -14,24 +14,7 @@ public class SumOfThree {
 			array[i] = sc.nextInt();
 		}
 		sc.close();
-		sum(n,array);
+		
 	}
 	
-	public static void sum(int n, int[] a) {
-		boolean flag = false;
-		int count = 0;
-		for(int i=0;i<n-2;i++)
-			for(int j=i+1;j<n-1;j++)
-				for(int k=j+1;k<n;k++) {
-					if(a[i]+a[j]+a[k]==0) {
-						count++;
-						System.out.println("The triples are ("+a[i]+","+a[j]+","+a[k]+")");
-						flag = true; 
-					}
-				}
-		if(flag == false) {
-			System.out.println("Triples not found");
-		}
-		System.out.println("total number of triples : "+count);
-	}
 }
