5634ec2230c3b8193bac40771b59481e65df062e
Saneeth S <saneeths@125AMBBLR-Chethan-RV.local>
2021-08-25 07:59:52 +0530

    Changes made in Sum of three is zero program

--- src/com/functionalcode/SumOfThree.java
+++ src/com/functionalcode/SumOfThree.java
@@ -13,8 +13,29 @@ public class SumOfThree {
 		for(int i=0;i<n;i++) {
 			array[i] = sc.nextInt();
 		}
+		sum(n,array);
 		sc.close();
 		
+	}
+	public static void sum(int n,int a[]) {
+		boolean flag = false;
+		int count = 0;
+		for(int i=0; i<n-2;i++) {
+			for(int j=i+1;j<n-1;j++) {
+				for(int k=j+1;j<n;j++) {
+					if(a[i]+a[j]+a[k]==0) {
+						count++;
+						System.out.println("Triples are ("+a[i]+","+a[j]+","+a[k]+")");
+						flag = true;
+					}	
+				}
+			}
+		}
+		
+		if(flag==false) {
+			System.out.println("No triples found");
+		}
+		System.out.println("No. of triplet pairs : "+count);
 	}
 	
 }
